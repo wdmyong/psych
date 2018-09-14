53507dd0d00bebf2bde6a05074d45d2dde46e609
duanyong <duanyong@kuaishou.com>
2018-09-14 11:51:36 +0800

    create project

---
+++ psych-api/src/main/java/com/wdm/psych/PsychApplication.java
@@ -0,0 +1,13 @@
+package com.wdm.psych;
+
+/**
+ * Hello world!
+ *
+ */
+public class PsychApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
